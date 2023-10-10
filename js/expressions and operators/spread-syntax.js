ab3c0b1ae0ad66ecfcac38f1ee899e1ff12bbd03
Gagandeep Sidhu <gagandeep.sidhu@dbschenker.com>
2023-10-10 12:24:53 -0400

    chore(): comment out non working code

--- js/expressions and operators/spread-syntax.js
+++ js/expressions and operators/spread-syntax.js
@@ -10,8 +10,8 @@ console.log(sum(...numbers));
 console.log(sum.apply(null, numbers));
 // output: 6
 
-const obj = { key1: "value1" };
-const array = [...obj]; // TypeError: obj is not iterable
-
-const array = [1, 2, 3];
-const obj = { ...array }; // { 0: 1, 1: 2, 2: 3 }
+// const obj = { key1: "value1" };
+// const array = [...obj]; // TypeError: obj is not iterable
+//
+// const array = [1, 2, 3];
+// const obj = { ...array }; // { 0: 1, 1: 2, 2: 3 }
